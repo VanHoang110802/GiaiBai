05350fedeae5295129626f471df09926fee1a258
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2023-06-06 22:16:39 +0700

    Create main.cpp

---
+++ CF_LV01/CF_LV01_02C/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  long long a; cin >> a;
+  cout << a << " mod 10 = " << a%10;
+  cout << '\n' << a << " div 10 = " << a/10;
+  return 0;
+}
